d05bb3e1198420267523a6a7854fce8297c6aedb
manfacegirl-create <manfacegirl@gmail.com>
2026-04-11 21:04:45 +0800

    app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 # ================= GEMINI SETUP =================
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model_ai = genai.GenerativeModel("gemini-1.0-pro")
+model_ai = genai.GenerativeModel("gemini-1.5-flash")
 
 # ================= SIDEBAR =================
 st.sidebar.title("🌿 Plant AI System")
